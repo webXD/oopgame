215ece9a7988ebdaac6ffc8695ddd79bc1904059
LF <2553003562@qq.com>
2017-07-20 23:00:48 +0800

    Revert "打气球"
    This reverts commit 818fc3385ec64e3a9899cf3659bac1d121f9e024.

--- bg.js
+++
@@ -1,7 +0,0 @@
-var bg = {
-	image : "./img/bg.jpg",
-	init:function(){
-		document.body.style.background = "url(" + this.image + ") no-repeat"; 
-		document.body.style.backgroundSize = "100% 100%"
-	}
-}
